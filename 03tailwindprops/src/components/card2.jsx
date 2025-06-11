81ddfd04589e192210b2cbbbabe141fb1a1cc7ee
Pranav-7262 <varpepranav8@gmail.com>
2025-06-11 09:41:49 +0530

    updated card2.jsx

--- 03tailwindprops/src/components/card2.jsx
+++ 03tailwindprops/src/components/card2.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { use } from "react";
 
-function card2() {
+function card2({ userName }) {
   return (
     <div>
       card2
       <p className="mt-2 text-gray-500">
         Looking to take your team away on a retreat to enjoy awesome food and
         take in some sunshine? We have a list of places to do just that.
+        {userName}
       </p>
     </div>
   );
